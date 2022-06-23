cdd3ad4633f0586507b42c7e72317f96a60bbf5e
PedroCaetano1995 <88352152+PedroCaetano1995@users.noreply.github.com>
2022-06-22 22:07:01 -0300

    Add files via upload

---
+++ VA3/VA3-A.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Tenneco
+ */
+import java.util.ArrayList;
+public class FichaDoAluno {
+    
+    
+    AvaliacaoFisica peso = new AvaliacaoFisica();
+    AvaliacaoFisica altura = new AvaliacaoFisica();
+    AvaliacaoFisica imc = new AvaliacaoFisica();
+    ArrayList avaliacoes = new ArrayList();
+
+   
+    
+    
+      public void relatorio() {
+        System.out.print(imc );
+    
+}
+}
